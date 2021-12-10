5f08ea1653b735b7c5807289a834346b7607b7fc
Rafael Fernandes Lopes <rafael.fernandes@gmail.com>
2021-12-10 01:40:31 -0300

    feat: tela de entrada

---
+++ lib/src/get_started/get_started_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:ifood_clone/src/components/action_button.dart';
+
+class GetStartedScreen extends StatelessWidget {
+  const GetStartedScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      backgroundColor: const Color(0xFFFF4B3A),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: screenWidth * 0.12),
+                child: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 37,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset("assets/images/logo.png"),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 40),
+              Padding(
+                padding: EdgeInsets.only(left: screenWidth * 0.12),
+                child: Text(
+                  "Food for Everyone",
+                  style: Theme.of(context).textTheme.headline1,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Image.asset("assets/images/toy_faces.png", width: screenWidth),
+              const SizedBox(height: 40),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ActionButton(
+                    "Get Started",
+                    backgroundColor: Colors.white,
+                    foregroundColor: const Color(0xFFFF460A),
+                    onPressed: () => false,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/icabanas_app.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:ifood_clone/src/get_started/get_started_screen.dart';
+
+class ICabanasApp extends StatelessWidget {
+  const ICabanasApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "iCabañas",
+      home: const GetStartedScreen(),
+      theme: ThemeData(
+        primaryColor: const Color(0xFFFA4A0C),
+        textTheme: const TextTheme(
+          headline1: TextStyle(
+            fontFamily: "SFPro",
+            fontSize: 62,
+            height: 0.8,
+            fontWeight: FontWeight.w800,
+            color: Color(0xFFFFFFFF),
+          ),
+          button: TextStyle(
+            fontFamily: "SFPro",
+            fontSize: 17,
+            fontWeight: FontWeight.w600,
+            color: Color(0xFFF6F6F9),
+          ),
+        ),
+      ),
+    );
+  }
+}
